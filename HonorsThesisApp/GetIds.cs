b162c4253613b5ab7612fe67d91ce47d4d591ce1
Rocki Taub <rockitaub@gmail.com>
2024-12-12 12:50:17 -0500

    Anna's changes for stage3

--- HonorsThesisApp/GetIds.cs
+++ HonorsThesisApp/GetIds.cs
@@ -1,5 +1,10 @@
 ﻿using System.Configuration;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Xml.Linq;
 using Microsoft.Data.SqlClient;
+using Microsoft.IdentityModel.Tokens;
+using static System.ComponentModel.Design.ObjectSelectorEditor;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement.Tab;
 
 namespace HonorsThesisApp
@@ -104,5 +109,85 @@ namespace HonorsThesisApp
             }
             return product;
         }
+
+        //holds all the info for a a store: key string is the name of the store, ProductStoreInfo is a class taht has the total price and list of products.
+        Dictionary<string, ProductStoreInfo> map = new Dictionary<string, ProductStoreInfo>();
+        List<string> noProductsAvailable = new List<string>();
+
+        //adds all the selectedStores to a dictionary 
+        public void addStores(List<string> selectedStores)
+        {
+            List<string> emptyList = new List<string>();
+            Tuple<double, string> emptyTuple = new Tuple<double, string>(0, "");
+            for (int ix = 0; ix < selectedStores.Count; ix++)
+            {
+                map.Add(selectedStores[ix], new ProductStoreInfo(0.0, emptyList));
+            }
+        }
+
+
+        //This method goes through every product and finds the cheapest one. It adds the product to the store list and increments the total price for that store
+        public void getStoreProducts(List<string> items, List<string> stores)
+        {
+            //do this for every product
+            for (int itemIX = 0; itemIX < items.Count; itemIX++)
+            {
+                string cheapestStore = "";
+                double cheapestPrice = 0;
+                string itemName = items[itemIX];
+                string getStoreAndPrice = "SELECT TOP 1 store_name FROM Store_products sp JOIN Stores s ON s.store_id = sp.store_id JOIN Products p ON sp.product_id = p.product_id " +
+                    "WHERE product_name = @product_name AND store_name IN(@store_names) BY price ASC";
+               
+                using (SqlConnection cnn = new SqlConnection(connString))
+                {
+                    try
+                    {
+                        // Open the connection to the database. 
+                        cnn.Open();
+                        
+                        using (SqlCommand cmd = new SqlCommand(getStoreAndPrice, cnn))
+                        {
+                            cmd.Parameters.AddWithValue("@product_name", itemName);
+                            cmd.Parameters.AddWithValue("@store_names", string.Join(", ", stores));
+
+                            try
+                            {
+                                using (SqlDataReader reader = cmd.ExecuteReader())
+                                {
+                                    if (reader.Read())
+                                    {
+                                        cheapestStore = reader["store_name"].ToString();
+                                        cheapestPrice = Convert.ToDouble(reader["price"]);
+                                    }
+                                }
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.WriteLine("Error executing query: " + ex.Message);
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("ERROR:" + ex.Message);
+                    }
+                }
+                
+                if(cheapestStore.IsNullOrEmpty() || cheapestPrice < 0.01)
+                {
+                    noProductsAvailable.Add(itemName);
+                }
+                else
+                {
+                    ProductStoreInfo info = map.GetValueOrDefault(cheapestStore);
+                    info.totalPrice += cheapestPrice;
+                    info.productNames.Add(itemName);
+                }
+
+
+            }
+        }
+
+       
     }
 }
--- HonorsThesisApp/ProductStoreInfo.cs
+++ HonorsThesisApp/ProductStoreInfo.cs
@@ -1,6 +1,14 @@
 ﻿namespace HonorsThesisApp
 {
-    internal class ProductStoreInfo
+    public class ProductStoreInfo
     {
+        public double totalPrice = 0.0;
+        public List<string> productNames = new List<string>();
+        public ProductStoreInfo(double totalPrice, List<string> productNames)
+        {
+            this.totalPrice = totalPrice;
+            this.productNames = productNames;
+
+        }
     }
 }
